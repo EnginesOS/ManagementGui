ce6adbc251feefca66c5021509ff3693dd91246e
Lachlan Douglas <lachlan@engines.org>
2017-03-23 16:00:20 +1100

    system response detail in fatal error report

--- app/models/bug_report.rb
+++ app/models/bug_report.rb
@@ -10,13 +10,14 @@ class BugReport
       schema: {
         type: :exception_report,
         version: {major: 0, minor: 1},
-        origin: 'Engines Manager v0.3'
+        origin: 'Engines System Manager 0.3'
       },
       exception: {
         class: @error.class,
         message: error_message,
         backtrace: application_backtrace,
-        detail: { params: @context.params.inspect }
+        detail: { params: @context.params.inspect,
+                  system_response: system_response }
       }
     }
   end
@@ -29,6 +30,12 @@ class BugReport
 
   def application_backtrace
     @error.backtrace.map{ |line| line.sub(Rails.root.to_s, '').split(':in ')[0] }.select{ |line| line.split('/')[1] == "app" }
+  end
+
+  def system_response
+    JSON.parse(@error.response)
+  rescue
+    'n/a'
   end
 
 end
