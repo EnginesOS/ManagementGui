74a2bdeab0cbc880443a923642c43fe1b7ad2cef
Lachlan Douglas <lachlan@engines.org>
2017-06-23 14:03:33 +1000

    rescue when redis down

--- app/jobs/engines_system_view_update_job.rb
+++ app/jobs/engines_system_view_update_job.rb
@@ -4,6 +4,8 @@ class EnginesSystemViewUpdateJob < ApplicationJob
 
   def perform(engines_system, message=nil)
     ActionCable.server.broadcast "engines_system_view_update_channel_#{engines_system.id}", html: html(engines_system, message)
+  rescue Redis::TimeoutError
+    # doesn't work when redis is down/restarting/paused
   end
 
   def html(engines_system, message)
