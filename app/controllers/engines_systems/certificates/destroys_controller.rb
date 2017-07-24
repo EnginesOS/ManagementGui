b50fcf9b2313f8fbf3718a917b8f8b3a6a045a2b
Lachlan Douglas <lachlan@engines.org>
2017-07-24 18:00:14 +1000

    service action output

--- app/controllers/engines_systems/certificates/destroys_controller.rb
+++ app/controllers/engines_systems/certificates/destroys_controller.rb
@@ -8,10 +8,10 @@ module EnginesSystems
         @certificate_delete = @engines_system.build_certificate_delete(certificate_path: params[:certificate_path])
         if @certificate_delete.update_system
           flash.now[:notice] = 'The certificate has been successfully deleted from the system.'
-          render 'engines_systems/certificates/index'
+          render 'engines_systems/certificates/manages/show'
         else
           flash.now[:alert] = 'Failed to delete the certificate from the system.'
-          render 'engines_systems/certificates/index'
+          render 'engines_systems/certificates/manages/show'
         end
       end
 
